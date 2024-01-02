0732be47e8062d69a103e6a5185bd561502b3ab9
Robert Dzienio <55928400+rdzienio@users.noreply.github.com>
2024-01-02 21:45:02 +0100

    added renting and returning

--- src/main/java/pl/gienius/reader/Menu.java
+++ src/main/java/pl/gienius/reader/Menu.java
@@ -1,29 +1,53 @@
 package pl.gienius.reader;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
 
     private Reader reader;
+    List<Book> books = new ArrayList<>();
+    List<Long> rented = new ArrayList<>();
 
     private final ReaderClient client = new ReaderClient();
 
-    private void header(){
+    private void header() {
         System.out.println("### Reader Client app ###");
     }
 
-    public void init(){
+    public void init() {
         header();
         createReaderMenu();
         menu();
     }
 
-    public void menu(){
-        header();
-
+    public void menu() {
+        while (true) {
+            printMainMenu();
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    getAvailableBooks();
+                    break;
+                case 2:
+                    rentBookMenu();
+                    break;
+                case 3:
+                    returnBookMenu();
+                    break;
+                case 0: {
+                    System.out.println("Exiting...");
+                    return;
+                }
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
     }
 
-    private void createReaderMenu(){
+    private void createReaderMenu() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Create new reader! What's your name?");
         String name = scanner.nextLine();
@@ -32,5 +56,76 @@ public class Menu {
         }
         reader = client.createReader(name);
         System.out.println("New Reader: " + reader);
+    }
+
+    private void printMainMenu() {
+        header();
+        System.out.println("1. Show all books");
+        System.out.println("2. Rent a book");
+        System.out.println("3. Return a book");
+        System.out.println("0. Exit");
+        System.out.print("Choose an option: ");
+    }
+
+    private void rentBookMenu() {
+        printAllBooks();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which book to rent: ");
+        Long bookId = scanner.nextLong();
+        if (client.rentBook(bookId, reader.getId())) {
+            rented.add(bookId);
+            System.out.println("Rented the book: " + bookId);
+        } else System.out.println("The book is blocked or something went wrong...");
+    }
+
+    private void getAvailableBooks() {
+        printAllBooks();
+    }
+
+    private void printAllBooks() {
+        books = client.getAvailableBooks();
+        if (books == null || books.isEmpty())
+            System.out.println("No books");
+        else {
+            for (Book book : books) {
+                System.out.println("Id: " + book.getId());
+                System.out.println("\tTitle: " + book.getTitle());
+                System.out.println("\tRelease date: " + book.getReleaseDate());
+                System.out.println("\tDescription: " + book.getDescription());
+                System.out.println("\n");
+            }
+        }
+    }
+
+    private void printRentedBooks(){
+        if(rented.isEmpty()){
+            System.out.println("List is empty!");
+        }
+        else {
+            System.out.println("List of books is:");
+            for(Long bookId : rented){
+                for (Book book : books) {
+                    if(bookId.equals(book.getId())) {
+                        System.out.println("Id: " + book.getId());
+                        System.out.println("\tTitle: " + book.getTitle());
+                        System.out.println("\tRelease date: " + book.getReleaseDate());
+                        System.out.println("\tDescription: " + book.getDescription());
+                        System.out.println("\n");
+                    }
+                }
+            }
+        }
+    }
+
+    private void returnBookMenu(){
+        printRentedBooks();
+        if(rented.isEmpty()) return;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which book to return: ");
+        Long bookId = scanner.nextLong();
+        if (client.returnBook(bookId, reader.getId())) {
+            rented.remove(bookId);
+            System.out.println("Returned the book: " + bookId);
+        } else System.out.println("Something went wrong...");
     }
 }
--- src/main/java/pl/gienius/reader/ReaderClient.java
+++ src/main/java/pl/gienius/reader/ReaderClient.java
@@ -2,11 +2,12 @@ package pl.gienius.reader;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 public class ReaderClient {
 
@@ -31,4 +32,66 @@ public class ReaderClient {
         logger.info("Sent request to addReader " + name);
         return response.getBody();
     }
+
+    public List<Book> getAvailableBooks(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String availableUrl = baseUrl + "available";
+
+        HttpEntity<?> request = new HttpEntity<>(headers);
+
+        ResponseEntity<List<Book>> response = restTemplate.exchange(
+                availableUrl,
+                HttpMethod.GET,
+                request,
+                new ParameterizedTypeReference<List<Book>>() {});
+        logger.info("Got available books");
+        return response.getBody();
+    }
+
+    public boolean rentBook(Long bookId, Long readerId){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Reader-ID", readerId.toString());
+
+        String rentUrl = readerUrl + "rent/" + bookId;
+
+        HttpEntity<?> request = new HttpEntity<>(headers);
+
+        try {
+            ResponseEntity<Void> response = restTemplate.postForEntity(rentUrl, request, Void.class);
+            logger.info("Book rented: " + bookId);
+            return response.getStatusCode() == HttpStatus.OK;
+        } catch (HttpClientErrorException e) {
+            if (e.getStatusCode() == HttpStatus.CONFLICT){
+                logger.info("Book: " + bookId + "is blocked!");
+                return false;
+            }
+            else throw e;
+        }
+    }
+
+    public boolean returnBook(Long bookId, Long readerId){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Reader-ID", readerId.toString());
+
+        String rentUrl = readerUrl + "rent/" + bookId;
+
+        HttpEntity<?> request = new HttpEntity<>(headers);
+
+        try {
+            ResponseEntity<Void> response = restTemplate.exchange(rentUrl, HttpMethod.DELETE, request, Void.class);
+            logger.info("Book rented: " + bookId);
+            return response.getStatusCode() == HttpStatus.OK;
+        } catch (HttpClientErrorException e) {
+            if (e.getStatusCode() == HttpStatus.CONFLICT){
+                logger.info("Something went wrong when returning the book: " + bookId + "...");
+                return false;
+            }
+            else throw e;
+        }
+    }
+
 }
